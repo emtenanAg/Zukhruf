25c0d2f64c46d0ddc2c898c23ce3a4da90eb81b4
Emtenan <emtenan.ag@gmail.com>
2023-05-14 15:08:50 +0300

    commit

--- zukhruf/lib/main.dart
+++ zukhruf/lib/main.dart
@@ -4,7 +4,9 @@ import './welcome.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
